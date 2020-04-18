0d530142463f2063d3d25a56fc8d96dd65a75eff
ronething <axingfly@gmail.com>
2020-04-18 17:09:09 +0800

    feat: reflect

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -65,6 +65,22 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Reykjavík"},
 		},
+		{
+			"Arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+		// {
+		// 	"Maps",
+		// 	map[string]string{
+		// 		"Foo": "Bar",
+		// 		"Baz": "Boz",
+		// 	},
+		// 	[]string{"Bar", "Boz"},
+		// },
 	}
 
 	for _, test := range cases {
@@ -78,5 +94,33 @@ func TestWalk(t *testing.T) {
 				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
 			}
 		})
+	}
+
+	// map 顺序不一致
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+}
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %+v to contain '%s' but it didnt", haystack, needle)
 	}
 }
